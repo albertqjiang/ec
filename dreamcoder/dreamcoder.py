11176a29a4a2be0a288dbbbb0289989811045de4
Albert Jiang <albert594250@gmail.com>
2020-05-28 20:11:18 -0400

    record all programs

--- dreamcoder/dreamcoder.py
+++ dreamcoder/dreamcoder.py
@@ -1,5 +1,5 @@
 import datetime
-
+import json
 import dill
 
 from dreamcoder.compression import induceGrammar
@@ -361,8 +361,9 @@ def ecIterator(grammar, tasks,
             
         sys.exit(0)
     
-    all_hit_programs = set() 
+    all_hit_programs = list() 
     for j in range(resume or 0, iterations):
+        iter_programs = list()
         if storeTaskMetrics and rewriteTaskMetrics:
             eprint("Resetting task metrics for next iteration.")
             result.recognitionTaskMetrics = {}
@@ -407,7 +408,7 @@ def ecIterator(grammar, tasks,
                                                       evaluationTimeout=evaluationTimeout)
             hit_programs = Frontier.output_hit_programs(topDownFrontiers)
             for h_program in hit_programs:
-                all_hit_programs.add(h_program)
+                iter_programs.append(h_program)
             result.trainSearchTime = {t: tm for t, tm in times.items() if tm is not None}
         else:
             eprint("Skipping top-down enumeration because we are not using the generative model")
@@ -486,9 +487,9 @@ def ecIterator(grammar, tasks,
 
             graphPrimitives(result, "%s_primitives_%d_"%(outputPrefix,j))
             
-        with open("hit_programs.txt", "w") as fhand:
-            for hit_program in sorted(list(all_hit_programs)):
-                fhand.write(hit_program+"\n")
+        with open("hit_programs.json", "w") as fhand:
+            all_hit_programs.append(iter_programs)
+            json.dump(all_hit_programs, fhand)
         yield result
 
 
